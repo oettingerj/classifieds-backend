f256d4b0a0aac15387bc4b6e0a550ae9688992c2
John Mullan <mullanj2@carleton.edu>
2020-02-26 20:42:45 -0600

    models

--- classifieds/sample/models.py
+++ classifieds/sample/models.py
@@ -1,6 +1,40 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField('Name', max_length=240)
+    email = models.EmailField()
+    roleChoice = models.TextChoices('roleChoice', 'STUDENT FACULTY STAFF')
+    role = models.CharField(choices=roleChoice.choices, max_length=7)
+
+    def __str__(self):
+        return self.name
+
+class Posting(models.Model):
+    user = models.ForeignKey(User)
+    timePosted = models.DateTimeField()
+    category = models.CharField() #change to choices later
+    prospective = models.BooleanField()
+    fulfilled = models.BooleanField()
+    description = models.TextField()
+    audienceChoice = models.TextChoices('audienceChoice', 'STUDENT FACULTY STAFF')
+    audience = models.CharField(choices=audienceChoice.choices, max_length=7)
+
+class RidePosting(models.Model):
+    posting = models.OneToOneField(Posting)
+    dateTimeOfRide = models.DateTimeField()
+    startLocation = models.TextField()
+    endLocation = models.TextField()
+    numberOfPeople = models.IntegerField()
+
+class ItemPosting(models.Model):
+    posting = models.OneToOneField(Posting)
+    images = models.ImageField()
+    price = models.DecimalField()
+    forSale = models.BooleanField()
+    forLoan = models.BooleanField()
+
+class Comment(models.Model):
+    posting = models.ForeignKey(Posting)
+    user = models.ForeignKey(User)
+    timePosted = models.DateTimeField()
+    content = models.TextField()
