5e0682c0234e8a6063b8bda3491321d753c74e0e
jamullan <jamullan@comcast.net>
2020-02-28 18:20:46 -0600

    installed djangorestframework

--- classifieds/sample/models.py
+++ classifieds/sample/models.py
@@ -10,9 +10,9 @@ class User(models.Model):
         return self.name
 
 class Posting(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     timePosted = models.DateTimeField()
-    category = models.CharField() #change to choices later
+    category = models.CharField(max_length=240) #change to choices later
     prospective = models.BooleanField()
     fulfilled = models.BooleanField()
     description = models.TextField()
@@ -20,21 +20,21 @@ class Posting(models.Model):
     audience = models.CharField(choices=audienceChoice.choices, max_length=7)
 
 class RidePosting(models.Model):
-    posting = models.OneToOneField(Posting)
+    posting = models.OneToOneField(Posting, on_delete=models.DO_NOTHING)
     dateTimeOfRide = models.DateTimeField()
     startLocation = models.TextField()
     endLocation = models.TextField()
     numberOfPeople = models.IntegerField()
 
 class ItemPosting(models.Model):
-    posting = models.OneToOneField(Posting)
+    posting = models.OneToOneField(Posting, on_delete=models.DO_NOTHING)
     images = models.ImageField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     forSale = models.BooleanField()
     forLoan = models.BooleanField()
 
 class Comment(models.Model):
-    posting = models.ForeignKey(Posting)
-    user = models.ForeignKey(User)
+    posting = models.ForeignKey(Posting, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     timePosted = models.DateTimeField()
     content = models.TextField()
