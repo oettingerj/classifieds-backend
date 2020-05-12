0717798490351543df5184e4694d38e683265c43
Josh Oettinger <joshkyle98@gmail.com>
2020-05-12 14:06:18 -0500

    Return user info on login

--- classifieds_project/auth_app/views.py
+++ classifieds_project/auth_app/views.py
@@ -12,8 +12,14 @@ def authenticate(request):
     instance_of_AuthBackend_class = AuthBackend()
     user = instance_of_AuthBackend_class.authenticate(request)
     csrf_token = csrf.get_token(request)
-    if user != None:
-        response = Response(data={'csrfToken': csrf_token}, status=status.HTTP_200_OK)
+    if user is not None:
+        data = {
+            'name': f'{user.given_name} {user.last_name}',
+            'email': user.email,
+            'id': user.id,
+            'csrfToken': csrf_token
+        }
+        response = Response(data=data, status=status.HTTP_200_OK)
 
         return response
     else:
